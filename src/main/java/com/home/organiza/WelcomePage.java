2c30639dcb55f756d80abf552e36a2268bb0b224
midoI <ahmed.jardak@gmail.com>
2014-07-13 19:07:10 +0100

    first commit

---
+++ src/main/java/com/home/organiza/WelcomePage.java
@@ -0,0 +1,26 @@
+package com.home.organiza;
+
+import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+/**
+ *
+ * @author ajar
+ */
+@CDIUI
+public class WelcomePage extends UI {
+
+
+    public static final String ORGANIZA_WELCOME_PAGE = "organiza Welcome Page";
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setSizeFull();
+        String message = ORGANIZA_WELCOME_PAGE;
+        Label label = new Label(message);
+        setContent(new HorizontalLayout(label));
+    }
+}
