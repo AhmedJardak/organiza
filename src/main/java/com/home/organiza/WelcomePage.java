0706fb9298f8858df06c9a1cef360364fd9a347b
midoI <ahmed.jardak@gmail.com>
2014-08-12 22:24:28 +0100

    sample

--- src/main/java/com/home/organiza/WelcomePage.java
+++ src/main/java/com/home/organiza/WelcomePage.java
@@ -1,10 +1,12 @@
 package com.home.organiza;
 
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.data.Property;
+import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+
+import java.io.File;
 
 /**
  *
@@ -13,15 +15,39 @@ import com.vaadin.ui.UI;
 @CDIUI
 public class WelcomePage extends UI {
 
-
-    public static final String ORGANIZA_WELCOME_PAGE = "organiza Welcome Page";
+    public static final String C = "C:\\";
+    FilesystemContainer docs=createFileSystem(C);
+    Table fileList= new Table("",docs);
 
     @Override
     protected void init(VaadinRequest request) {
+        setContent(createContent());
         setSizeFull();
-
-        String message = ORGANIZA_WELCOME_PAGE;
-        Label label = new Label(message);
-        setContent(new HorizontalLayout(label));
     }
+
+    private VerticalLayout createContent() {
+        VerticalLayout content = new VerticalLayout();
+        TextField textField = createTextField();
+        content.addComponent(textField);
+        content.addComponent(fileList);
+        return content;
+    }
+
+    private TextField createTextField() {
+        TextField  textField = new TextField();
+        textField.setValue(C);
+        textField.setImmediate(true);
+        textField.addValueChangeListener((Property.ValueChangeEvent event) -> {
+            // Do something with the value
+            fileList.setContainerDataSource(createFileSystem((String) event.getProperty().getValue()));
+            Notification.show("Value is: " + event.getProperty().getValue());
+        });
+        return textField;
+    }
+
+    private FilesystemContainer createFileSystem(String path) {
+        return new FilesystemContainer(new File(path),false);
+    }
+
+
 }
